d0c1430311fc7538608fc7a68580b6b2820601f8
Umigishi-Aoi <umigishi.aoi@gmail.com>
2023-11-09 09:34:24 +0900

    Firebase initialization and add firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const ProviderScope(child: MyApp()));
 }
 
