8bbb6b3db380286eebd87bc4ff7329eed1ae684c
Umigishi-Aoi <umigishi.aoi@gmail.com>
2023-11-22 16:46:07 +0900

    add customer page

---
+++ lib/feature/core/presentation/customer_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:stripe_test/feature/signin/provider/auth_provider.dart';
+
+class CustomerPage extends ConsumerWidget {
+  const CustomerPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final notifier = ref.watch(authedUserProvider.notifier);
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () async {
+              await notifier.signOut();
+            },
+            icon: const Icon(Icons.logout),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const Text('You are the customer'),
+            Text(ref.watch(authedUserProvider)!.uid),
+          ],
+        ),
+      ),
+    );
+  }
+}
